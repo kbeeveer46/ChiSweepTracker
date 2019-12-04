c2e4f017242e198ff57e5ad3b84e1000cf56ac8c
Kyle Beverforden <kylebeverforden@gmail.com>
2019-12-04 15:43:04 -0600

    Initial Commit

---
+++ ChiSweepTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChiSweepTracker
+//
+//  Created by Macbook on 12/4/19.
+//  Copyright © 2019 Kyle Beverforden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
