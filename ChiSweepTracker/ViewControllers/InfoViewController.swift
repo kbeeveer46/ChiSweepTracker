d7755c6ddf6190472f1863e27f7991bfa9a44416
Kyle Beverforden <kylebeverforden@gmail.com>
2019-12-18 20:57:35 -0600

    Added contact button

--- ChiSweepTracker/ViewControllers/InfoViewController.swift
+++ ChiSweepTracker/ViewControllers/InfoViewController.swift
@@ -1,11 +1,13 @@
 import UIKit
+import MessageUI
 
-class InfoViewController: UIViewController {
+class InfoViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     @IBOutlet weak var infoButton: UIButton!
     @IBOutlet weak var signsButton: UIButton!
     @IBOutlet weak var ticketButton: UIButton!
-    
+	@IBOutlet weak var contactButton: UIButton!
+	
     let common = Common()
     
     override func viewWillAppear(_ animated: Bool) {
@@ -18,6 +20,23 @@ class InfoViewController: UIViewController {
         self.common.styleButton(infoButton, "sweeper", "007AFF")
         self.common.styleButton(signsButton, "warning", "FF7832")
         self.common.styleButton(ticketButton, "dollar_circle", "86A697")
+		self.common.styleButton(contactButton, "mail", "BF1A2F")
     }
 
+	@IBAction func contactButtonTapped(_ sender: Any) {
+		
+		if MFMailComposeViewController.canSendMail() {
+			let mail = MFMailComposeViewController()
+			mail.mailComposeDelegate = self
+			mail.setToRecipients(["contact@chicagosweeptracker.info"])
+			mail.setSubject("Chicago Sweep Tracker")
+			present(mail, animated: true)
+		} else {
+			self.common.showAlert(self.common.constants.errorTitle, "Unable to find email app")
+		}
+	}
+	
+	func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+		controller.dismiss(animated: true)
+	}
 }
