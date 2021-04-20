ef0d8b616fb571179543b2911f95805fdf8488f2
Kyle Beverforden <kylebeverforden@gmail.com>
2021-04-20 18:01:54 -0500

    Updates

---
+++ ChiSweepTracker/ViewControllers/FavoriteListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoriteListViewController.swift
+//  ChiSweepTracker
+//
+//  Created by Kyle Beverforden on 4/20/21.
+//  Copyright © 2021 Kyle Beverforden. All rights reserved.
+//
+
+import Foundation
